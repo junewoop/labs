dff7139a104bd645764351c6ff2e3f43816fc142
junewoop <40329273+junewoop@users.noreply.github.com>
2020-11-01 23:34:44 -0500

    Revert "Revert "After Task 9""
    This reverts commit 726bfedfa6357b4aea2c51be415c11b37aa8909b.

--- lab07/src/gl/textures/DepthBuffer.cpp
+++ lab07/src/gl/textures/DepthBuffer.cpp
@@ -9,6 +9,8 @@ DepthBuffer::DepthBuffer(int width, int height) :
     m_height(height)
 {
     // TODO [Task 8] bind() the render buffer and call glRenderbufferStorageEXT
-
+    bind();
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, m_width, m_height);
     // TODO Don't forget to unbind()!
+    unbind();
 }
